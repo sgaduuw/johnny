170660c175c30dc8efde22792cc69f870241999a
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2024-03-12 22:16:45 +0100

    Add config class for celery

--- johnny/config.py
+++ johnny/config.py
@@ -1,3 +1,16 @@
+from environs import Env
 
-class FlaskConfig:
-    DEBUG = True
+env = Env()
+
+class Celery:
+    CELERY_CONFIG = {
+        'broker_url': env.str('CELERY_BROKER_URL'),
+        'broker_connection_retry': True,
+        'broker_connection_retry_on_startup': True,
+        'worker_cancel_long_running_tasks_on_connection_loss': True,
+        'worker_prefetch_multiplier': 0
+    }
+
+
+class FlaskConfig(Celery):
+    DEBUG = env.bool('FLASK_DEBUG')
