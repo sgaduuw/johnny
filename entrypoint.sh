6a8bae050e1c16a4c2eeca646b61bb0c8a709925
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2024-04-03 23:57:31 +0200

    Add a few newlines for readability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 case "$1" in
+
   johnny)
     echo -n "Starting johnny.. "
     if [ $FLASK_DEBUG -ne 1 ]; then
@@ -11,18 +12,23 @@ case "$1" in
       /usr/local/bin/flask --app johnny:create_app run --host 0.0.0.0 --port 8000
     fi
   ;;
+
   worker)
     echo "WORKER"
     celery -A johnny.tasks:clry worker
   ;;
+
   scheduler)
     echo "SCHEDULER"
     celery -A johnny.tasks:clry beat -s /tmp/celerybeat-schedule
   ;;
+
   bash)
     bash
   ;;
+
   *)
     echo "Unknown command"
   ;;
+
 esac
